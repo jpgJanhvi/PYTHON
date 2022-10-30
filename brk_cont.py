8d7873bf26100c3a55d691232254907cbd0b0a14
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-10-31 00:43:15 +0530

    Add files via upload

---
+++ brk_cont.py
@@ -0,0 +1,10 @@
+students=["ram","shyam","kishan","radha","kishanika"]
+for student in students:
+    if student == "radha":
+        break;
+    print(student)
+
+for student in students:
+    if student == "kishan":
+        continue;
+    print(student)
