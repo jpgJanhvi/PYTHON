56b18fd050bb51f302c4f2b3017be37b6369bb42
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-10-31 00:42:59 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+#operators 
+print(5-2)
+print(5*2)
+print(float(5-2))
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+#shortcuts
+i=5
+i=i+2
+i+=2
+i-=2
+print(i)
+
+#operator precedence 
+result=2+3/5
+print(result)
+
+#here is a comment
+
