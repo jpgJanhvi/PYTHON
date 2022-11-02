005e96ce7af33bf34b04773a70438078388b23b6
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-11-02 23:35:24 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+# there are 3 types of funtions :
+# 1. In-Built Functions 
+# 2. Module functions 
+# 3. User-Defined functions 
+# in-built function:
+# int()
+# str()
+# bool()
+
+# import math
+# print(dir(math))
+
+# from math import *
+# print(sqrt(16))
+
+def print_sum(first,second):
+    print(first + second)
+print_sum(1,2)
