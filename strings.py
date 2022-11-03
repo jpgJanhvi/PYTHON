8c9830f5172661bc7d387cf9fdc8a756944e67cc
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-11-03 21:18:30 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,9 @@
+name="tony stark"
+print(name.upper())
+print(name.lower())
+print(name.find('s'))
+print(name.replace("tony stark","iron man"))
+print(name.replace("stark", "man"))
+print("t" in name)
+print("T" in name)
+
